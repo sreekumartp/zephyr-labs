7469b2bb517c8dc9c5554589bc333c44f408ee48
sreekumartp <sputnikspacemen@gmail.com>
2025-06-20 18:09:54 +0530

    feat: add shell commands to retrieve current door and water level states

--- apps/washing_machine_sim/src/shell_interface.c
+++ apps/washing_machine_sim/src/shell_interface.c
@@ -16,4 +16,18 @@ static int cmd_water(const struct shell *sh, size_t argc, char **argv){
 }
 SHELL_CMD_ARG_REGISTER(water, NULL, "water empty|full", cmd_water, 2, 0);
 
+static int cmd_door_state(const struct shell *sh, size_t argc, char **argv){
+    bool state = door_sensor_sim_get_state();
+    shell_print(sh, "Door is currently: %s", state ? "closed" : "open");
+    return 0;
+}
+SHELL_CMD_REGISTER(door_state, NULL, "Get current door state", cmd_door_state);
+
+static int cmd_water_state(const struct shell *sh, size_t argc, char **argv){
+    bool state = water_level_sim_get_state();
+    shell_print(sh, "Water level is currently: %s", state ? "full" : "empty");
+    return 0;
+}
+SHELL_CMD_REGISTER(water_state, NULL, "Get current water level", cmd_water_state);
+
 void wm_shell_init(void) { /* Trigger shell if needed */ }
